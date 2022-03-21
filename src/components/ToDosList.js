36d2aadf764f006dc2e52eb4d9210690f5a4c860
Brayan Leyva <brayanleyvag@gmail.com>
2022-03-20 20:47:37 -0600

    finished

--- src/components/ToDosList.js
+++ src/components/ToDosList.js
@@ -18,14 +18,12 @@ export const ToDosList = ({toDos, setTaskSelected, deleteTask, setToDos, setIsMo
         <table>
             <tbody>
             {toDos.map(toDo =>(
-                <tr key={toDo.id}>
-                    <td>
-                        <div className='status' onClick={() =>  changeStatus(toDo)}>{toDo.isCompleted ? 
+                <tr className='container-todo' key={toDo.id}>
+                    <td className='td-status' onClick={() =>  changeStatus(toDo)}>{toDo.isCompleted ? 
                             <div className='circle'><i className="fas fa-check"></i></div> : 
-                            <div className='circle'></div> }
-                        </div>
+                            <div className='circle'></div>}
                     </td>
-                    <td>
+                    <td className='td-text'>
                         <div className='task-text'>
                             <h3>{toDo.isCompleted? <strike>{toDo.title}</strike> : toDo.title}</h3>
                             <p>{toDo.isCompleted? <strike>{toDo.description}</strike> : toDo.description}</p>
@@ -33,8 +31,8 @@ export const ToDosList = ({toDos, setTaskSelected, deleteTask, setToDos, setIsMo
                     </td>
                     <td>
                         <div className='buttons-list'>
-                            <button className='btn-list btn-edit' onClick={() => {setTaskSelected(toDo); setIsModalOpen(true)}}>Edit</button>
-                            <button className='btn-list btn-delete' onClick={() => deleteTask(toDo.id)}>Delete</button>
+                            <button className='btn-list btn-edit' onClick={() => {setTaskSelected(toDo); setIsModalOpen(true)}}><i className="fas fa-edit"></i></button>
+                            <button className='btn-list btn-delete' onClick={() => deleteTask(toDo.id)}><i className="fas fa-trash"></i></button>
                         </div>
                     </td>
                 </tr>
